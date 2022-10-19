d01809d376d208dc1f1302161853305bc87e01fd
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-10-19 17:31:10 +0200

    commit

---
+++ main.py
@@ -0,0 +1,16 @@
+###### EJERCICIO 1 ####
+print("Vamos a calcular el área de un círculo")
+
+def area_circulo():
+    import math
+    r=int(input("Introduce un radio:"))
+    pi= math.pi
+    AREA=pi*(r^2)
+    print("Siendo la fórmula: pi*(r^2), el resultado del área, con radio {} es {}".format(r, AREA))
+area_circulo()
+
+
+###### EJERCICIO 2 ######
+
+print("\n\n\n")
+
